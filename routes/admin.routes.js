ff94c090588cfa8c3c9682ac02287826e6bcd120
chaitanya <dev.chaitanyasonawane@gmail.com>
2025-11-02 11:06:02 +0530

    skeleton routes of user, admin & course

---
+++ routes/admin.routes.js
@@ -0,0 +1,30 @@
+const {Router} = require('express')
+const adminRouter= Router()
+
+adminRouter.post('/signup', (req, res) => {
+
+})
+
+adminRouter.post('signin', (req, res) => {
+
+})
+
+adminRouter.post('/course', (req, res) => {
+
+})
+
+adminRouter.put('/course', (req, res) => {
+
+})
+
+adminRouter.get('/course/bulk', (req, res) => {
+    
+})
+
+adminRouter.post('/addcontent', (req, res) => {
+
+})
+
+module.exports = {
+    adminRouter : adminRouter
+}
